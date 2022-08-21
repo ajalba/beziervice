219f66a7eda677c738c0bb1215ee8e41478baa9f
ajalba <ajalba@correo.ugr.es>
2022-08-21 19:05:31 +0200

    implements function to curve from points and tests closes #6

--- Beziervice/src/bezier_curves/curves.rs
+++ Beziervice/src/bezier_curves/curves.rs
@@ -4,7 +4,8 @@ use crate::bezier_curves::point::Point;
 #[derive(Clone)]
 pub struct BezierCurve {
     points: Vec<Point>,
-    grade: i8
+    grade: i8,
+    expression: Polynomial<f64>
 }
 
 impl BezierCurve {
@@ -19,7 +20,7 @@ impl BezierCurve {
             let mut next_iter = vec![];
             for i in 1..n_puntos-j+1 {
                 let x: Polynomial<f64> = Polynomial::from_coef(vec![1.0, 0.0]);
-                let one_minus_x: Polynomial<f64> = Polynomial::from_coef(vec![1.0, -1.0]);
+                let one_minus_x: Polynomial<f64> = Polynomial::from_coef(vec![-1.0, 1.0]);
                 let pol_one_minus_x = aux_points[i-1].clone();
                 let pol_x = aux_points[i].clone();
                 next_iter.push(pol_one_minus_x * one_minus_x + pol_x * x)
@@ -38,4 +39,35 @@ impl BezierCurve {
         todo!()
     }
 
+}
+#[cfg(test)]
+mod tests {
+    use mathru::algebra::abstr::Polynomial;
+    use crate::bezier_curves::curves::BezierCurve;
+    use pretty_assertions::{assert_eq, assert_ne};
+    #[test]
+    fn add_polynomial() {
+        let pol_one = Polynomial::from_coef(vec![1.0, 0.0]);
+        let pol_two = Polynomial::from_coef(vec![0.0, 1.0]);
+        assert_eq!(pol_one + pol_two, Polynomial::from_coef(vec![1.0, 1.0]));
+    }
+    #[test]
+    fn simple_curve() {
+        let pol_one = Polynomial::from_coef(vec![2.0]);
+        let pol_two = Polynomial::from_coef(vec![1.0]);
+        let _vector_pol = vec![pol_one, pol_two];
+        let decas_pol = BezierCurve::generate_curve(_vector_pol);
+        assert_eq!(1, decas_pol.degree());
+        assert_eq!(decas_pol, Polynomial::from_coef(vec![-1.0, 2.0]));
+    }
+    #[test]
+    fn second_degree_curve() {
+        let pol_one = Polynomial::from_coef(vec![1.0]);
+        let pol_two = Polynomial::from_coef(vec![2.0]);
+        let pol_three = Polynomial::from_coef(vec![1.0]);
+        let vector_pol = vec![pol_one, pol_two, pol_three];
+        let decas_pol = BezierCurve::generate_curve(vector_pol);
+        assert_eq!(decas_pol, Polynomial::from_coef(vec![-2.0, 2.0, 1.0]));
+        assert_eq!(2, decas_pol.degree());
+    }
 }
