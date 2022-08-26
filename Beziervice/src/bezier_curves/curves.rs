2b513828a05c0871f4c2caeee61030b5d83894a1
ajalba <ajalba@correo.ugr.es>
2022-08-27 01:47:27 +0200

    generates continuous joined curves from several points closes #8

--- Beziervice/src/bezier_curves/curves.rs
+++ Beziervice/src/bezier_curves/curves.rs
@@ -12,10 +12,10 @@ impl BezierCurve {
     pub fn base_n_grade(grade: i32) -> String {
         todo!()
     }
-    pub fn generate_curve(points: Vec<Polynomial<f64>>) -> Polynomial<f64> {
-        let mut aux_points = points.clone();
+    pub fn generate_curve(_points: Vec<Polynomial<f64>>) -> Polynomial<f64> {
+        let mut aux_points = _points.clone();
         
-        let n_puntos = points.len();
+        let n_puntos = _points.len();
         for j in 1..n_puntos {
             let mut next_iter = vec![];
             for i in 1..n_puntos-j+1 {
@@ -40,8 +40,14 @@ impl BezierCurve {
         }
         return points;
     }
-    fn generate_curves(_points: Vec<Point>) -> Vec<BezierCurve> {
-        todo!()
+    fn generate_curves(_points: Vec<Polynomial<f64>>, _grade: i32) -> Vec<Polynomial<f64>> {
+        let mut curves = Vec::<Polynomial<f64>>::new();
+        let n_curves = _points.len() / _grade as usize;
+        for i in 0..n_curves {
+            let subvector: Vec<Polynomial<f64>>  = _points[i*_grade as usize..1+(i+1)*_grade as usize].to_vec();
+            curves.push(BezierCurve::generate_curve(subvector));
+        }
+        return curves
     }
     fn aprox_function(_function: String, _grade: i32) -> Vec<Point> {
         todo!()
@@ -53,6 +59,8 @@ impl BezierCurve {
 }
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use mathru::algebra::abstr::Polynomial;
     use crate::bezier_curves::curves::BezierCurve;
     use pretty_assertions::{assert_eq, assert_ne};
@@ -80,5 +88,24 @@ mod tests {
         let decas_pol = BezierCurve::generate_curve(vector_pol);
         assert_eq!(decas_pol, Polynomial::from_coef(vec![-2.0, 2.0, 1.0]));
         assert_eq!(2, decas_pol.degree());
+    }
+    #[test]
+    fn joins_two_curves() {
+        let first_curve = BezierCurve::generate_curve(vec![
+            Polynomial::from_coef(vec![1.0]),
+            Polynomial::from_coef(vec![2.0]),
+            Polynomial::from_coef(vec![1.0])]);
+        let second_curve = BezierCurve::generate_curve(vec![
+            Polynomial::from_coef(vec![1.0]),
+            Polynomial::from_coef(vec![2.0]),
+            Polynomial::from_coef(vec![1.0])]);
+        let two_curves = vec![first_curve, second_curve];
+        let vector_points = vec![
+            Polynomial::from_coef(vec![1.0]),
+            Polynomial::from_coef(vec![2.0]),
+            Polynomial::from_coef(vec![1.0]),
+            Polynomial::from_coef(vec![2.0]),
+            Polynomial::from_coef(vec![1.0])];
+        assert_eq!(BezierCurve::generate_curves(vector_points, 2), two_curves);
     }
 }
